e5d12bf9e4d1b6c632d89d7652d574e254c528fc
Huynh Thai Hoc <huynhhoc@gmail.com>
2023-09-04 08:53:22 +0200

    Update ISBSGModel.py
    feature mapping

--- ISBSGModel.py
+++ ISBSGModel.py
@@ -17,8 +17,21 @@ class ISBSGModel(nn.Module):
         self.relu = nn.ReLU()
         
     def forward(self, x):
+        # Apply feature mapping functions to x to adapt to the model's input size
+        x = self.feature_mapping(x)
         x = self.relu(self.fc1(x))
         x = self.relu(self.fc2(x))
         x = self.relu(self.fc3(x))
         x = self.fc4(x)
         return x
+    def feature_mapping(self, x):
+        # Implement feature mapping logic here
+        # This function should map x from the new dataset to the model's input size
+        # Example: If the new dataset has a different feature order or scaling, adjust it here
+        
+        # Assuming the input features are in the order EI, EO, EQ, EIF, ILF
+        # and the new dataset has them in a different order
+        # You can reorder them to match the model's input order
+        x_reordered = x[:, [3, 0, 1, 2, 4]]  # New order: EIF, EI, EO, EQ, ILF
+        # You can also apply any necessary scaling or transformations here
+        return x_reordered
